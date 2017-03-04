ef68cffd496f6c88cd14fae441e65af5a3bf8ced
tianhett <shuntianfeng@126.com>
2017-03-04 20:05:26 +0800

    小说爬取合并下载器

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace NovelDownloader
+{
+    partial class b
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressText = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "小说目录地址：";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(116, 28);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(430, 55);
+            this.txtSource.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "目标文件：";
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.Location = new System.Drawing.Point(116, 114);
+            this.txtTarget.Multiline = true;
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.ReadOnly = true;
+            this.txtTarget.Size = new System.Drawing.Size(430, 53);
+            this.txtTarget.TabIndex = 3;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(166, 230);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(94, 40);
+            this.btnDownload.TabIndex = 4;
+            this.btnDownload.Text = "下载";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(326, 230);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 40);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "退出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.Transparent;
+            this.progressBar1.Location = new System.Drawing.Point(116, 190);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(430, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // progressText
+            // 
+            this.progressText.AutoSize = true;
+            this.progressText.BackColor = System.Drawing.Color.Transparent;
+            this.progressText.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.progressText.ForeColor = System.Drawing.Color.Blue;
+            this.progressText.Location = new System.Drawing.Point(315, 194);
+            this.progressText.Name = "progressText";
+            this.progressText.Size = new System.Drawing.Size(26, 16);
+            this.progressText.TabIndex = 7;
+            this.progressText.Text = "0%";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "下载进度：";
+            // 
+            // b
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 292);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressText);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label1);
+            this.Name = "b";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "网页小说下载器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label progressText;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Threading;
+using HtmlAgilityPack;
+
+namespace NovelDownloader
+{
+    public partial class b : Form
+    {
+        public b()
+        {
+            InitializeComponent();
+            m_context = SynchronizationContext.Current;
+        }
+
+        SynchronizationContext m_context = null;
+        List<Thread> threadList = new List<Thread>();
+        List<string> filePathList = new List<string>();
+        public struct NovelDownloadStruct
+        {
+            public List<string> links;
+            public string targetFile;
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            progressText.Text = "0%";
+            if (!string.IsNullOrEmpty(txtSource.Text))
+            {
+                try
+                {
+                    string title = string.Empty;
+                    if (txtSource.Text.LastIndexOf("/") <= -1)
+                    {
+                        txtSource.Text += "/";
+                    }
+                    string htmlText = GetHtmlString(txtSource.Text);
+                    List<string> linkList = GetAllLinks(htmlText, ref title);
+                    txtTarget.Text = Directory.GetCurrentDirectory() + "\\" + title + ".txt";
+                    string targetFile = title + ".txt";
+                    //DownloadAndMerge(linkList, targetFile);
+                    NovelDownloadStruct novelStruct = new NovelDownloadStruct();
+                    novelStruct.links = linkList;
+                    novelStruct.targetFile = targetFile;
+                    Thread thread = new Thread(DownloadAndMerge);
+                    thread.IsBackground = true;
+                    thread.Start(novelStruct);
+                    threadList.Add(thread);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("检查地址是否正确");
+                }
+            }
+            else
+            {
+                MessageBox.Show("小说目录地址不能为空");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Application.Exit();
+            if (threadList.Count > 0)
+            {
+                threadList.First().Abort();
+            }
+            DeleteAllFile();
+            Environment.Exit(0);
+        }
+
+        private List<string> GetAllLinks(string htmlText, ref string title)
+        {
+            List<string> aLinks = new List<string>();
+            Regex regTitle = new Regex(@"<title>(.*)</title>");
+            var resTitle = regTitle.Match(htmlText).Groups;
+            title = resTitle[1].ToString();
+            Regex reg = new Regex(@"(?is)<a[^>]*?href=(['""]?)(?<url>[^'""\s>]+)\1[^>]*>(?<text>(?:(?!</?a\b).)*)</a>");
+            MatchCollection mc = reg.Matches(htmlText);
+            foreach (Match m in mc)
+            {
+                if (Path.GetExtension(m.Groups["url"].Value).IndexOf("html") > -1)
+                {
+                    Regex regPage = new Regex(@"第(\d+)章");
+                    var result = regPage.Match(m.Groups["text"].Value).Groups;
+                    if (result.Count > 1)
+                    {
+                        aLinks.Add(m.Groups["url"].Value);
+                    }
+                }
+            }
+            return aLinks;
+        }
+
+        private void DownloadAndMerge(object param)
+        {
+            NovelDownloadStruct novelStruct = (NovelDownloadStruct)param;
+            List<string> links = novelStruct.links;
+            string targetFile = novelStruct.targetFile;
+            string dir = Directory.GetCurrentDirectory() + "\\" + DateTime.Now.ToString("yyyyMMddhhmmssffff");
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            else
+            {
+                string cmd = " del /Q \"" + dir + "\\*.*\"";
+                string output = "";
+                CmdHelper.RunCmd(cmd, out output);
+            }
+            if (links.Count > 0)
+            {
+                double per = 0.95 / links.Count;
+                for (int i = 0; i < links.Count; i++)
+                {
+                    string eachPath = string.Empty;
+                    if (links[i].IndexOf("/") > -1)
+                    {
+                        Uri uri = new Uri(txtSource.Text);
+                        eachPath = uri.Scheme + "://" + uri.Host + links[i].Replace(txtSource.Text,"/");
+                    }
+                    else
+                    {
+                        eachPath = txtSource.Text + Path.GetFileName(links[i]);
+                    }
+                    string htmlText = GetHtmlString(eachPath);
+                    //htmlText = DealWithHtml(htmlText);
+                    htmlText = NoHTML(htmlText);
+                    string fileName = Path.GetFileNameWithoutExtension(eachPath);
+                    string path = dir + "\\" + fileName + ".txt";
+                    if (!filePathList.Contains(path))
+                    {
+                        filePathList.Add(path);
+                        MergeText(htmlText, path);
+                        int progress = (int)((i + 1) * per * 100);
+                        m_context.Post(UpdateUI, progress);
+                    }
+                }
+                //string cmd = @"copy *.txt temp.txt";
+                string cmd = "copy \"" + dir + "\\*.txt\" \"" + dir + "\\temp.txt\"";
+                string output = "";
+                CmdHelper.RunCmd(cmd, out output);
+                //File.Move("temp.txt", targetFile);
+                File.Move(dir + "\\temp.txt", targetFile);
+                DeleteAllFile();
+                m_context.Post(UpdateUI, 100);
+                MessageBox.Show("下载完成");
+            }
+        }
+
+        private string NoHTML(string Htmlstring)
+        {
+            //删除脚本  
+            Htmlstring = Regex.Replace(Htmlstring, @"<script[^>]*?>.*?</script>", "", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"<script[^>]*?>[\s\S]*?</script>", "", RegexOptions.IgnoreCase);
+            //删除HTML的head
+            Htmlstring = Regex.Replace(Htmlstring, @"<head>[\s\S]*</head>", "", RegexOptions.IgnoreCase);
+            //删除HTML 
+            Htmlstring = Regex.Replace(Htmlstring, @"<(.[^>]*)>", "", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"([\r\n])[\s]+", "", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"-->", "", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"<!--.*", "", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(quot|#34);", "\"", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(amp|#38);", "&", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(lt|#60);", "<", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(gt|#62);", ">", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(nbsp|#160);", "   ", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(iexcl|#161);", "\xa1", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(cent|#162);", "\xa2", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(pound|#163);", "\xa3", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(copy|#169);", "\xa9", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&#(\d+);", "", RegexOptions.IgnoreCase);
+            Htmlstring.Replace("<", "");
+            Htmlstring.Replace(">", "");
+            Htmlstring.Replace("\r\n", "");
+            Htmlstring = Htmlstring.Replace("<br/>", "").Replace("<br>", "").Trim();
+            return Htmlstring;
+        }
+
+        //private string DealWithHtml(string Htmlstring)
+        //{
+        //    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+        //    doc.LoadHtml(Htmlstring);
+
+        //    return "";
+        //}
+
+        private void MergeText(string htmlText, string path)
+        {
+            try
+            {
+                FileStream fs = fs = new FileStream(path, FileMode.Create);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(htmlText);
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DeleteAllFile()
+        {
+            if (filePathList.Count > 0)
+            {
+                foreach (var each in filePathList)
+                {
+                    File.Delete(each);
+                }
+            }
+        }
+
+        private void UpdateUI(object value)
+        {
+            progressBar1.Value = Convert.ToInt32(value);
+            progressText.Text = string.Format("{0}%", value);
+        }
+
+        private string GetHtmlString(string url)
+        {
+            WebClient myWebClient = new WebClient();
+            byte[] myDataBuffer = myWebClient.DownloadData(url);
+            string strWebData = Encoding.Default.GetString(myDataBuffer);
+            string pattern = @"(?i)\bcharset=(?<charset>[-a-zA-Z_0-9]+)";
+            string charset = Regex.Match(strWebData, pattern).Groups["charset"].Value;
+            if (!string.IsNullOrEmpty(charset) && Encoding.GetEncoding(charset) != Encoding.Default)
+            {
+                strWebData = Encoding.GetEncoding(charset).GetString(myDataBuffer);
+            }
+            else
+            {
+                strWebData = Encoding.Default.GetString(myDataBuffer);
+            }
+            return strWebData; 
+        }
+
+    }
+}
